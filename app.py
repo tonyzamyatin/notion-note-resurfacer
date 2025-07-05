25018a20f6c3af4ffb0504189389e8bdac1230e9
Anton Zamyatin <anton.zamyatin14@gmail.com>
2025-07-05 10:34:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,60 +5,44 @@ import re
 
 """ Config data """
 
-# Enter your integration token here
 INTEGRATION_TOKEN = ""
-# Enter your database id here
 NOTION_DATABASE_ID = ""
-# Enter the number of pages you want to randomly retrieve
-num_random_pages = 3
-# Enter here the name of the checkbox property
-prop_name = ""
+NUM_RANDOM_PAGES = 3
+PROP_NAME = "Today's idea"
+
+""" Functions. Do not change anything below unless you want to change the logic """
 
 
-""" Functions. Do not change anything here unless you want to modify how the program works """
-
-
-def queryDatabase(database_id, token):
-    # Because the limit of pages one can query per request is 100 we loop through the database and make multiple querries to get all pages.
+def query_database(database_id, token):
     has_more = None
     next_cursor = None
     url = f"https://api.notion.com/v1/databases/{database_id}/query"
 
-    while (has_more == None or has_more == True):
-        # If there is no "next_curser" (e.g. it's the first or only query) make a default request
-        if (next_cursor == None):
-            db = getDatabaseSection(url, token)
-
-        # If there is a "next_cursor" use it as "start_cursor" for the next request
+    while has_more is None or has_more:
+        if next_cursor is None:
+            db = get_database_section(url, token)
         else:
-            db_section = getDatabaseSection(url, token, next_cursor)
+            db_section = get_database_section(url, token, next_cursor)
             db["results"].extend(db_section["results"])
             db["has_more"] = db_section["has_more"]
             db["next_cursor"] = db_section["next_cursor"]
 
-        # Update iteration variables
         has_more = db["has_more"]
-        if (has_more):
+        if has_more:
             next_cursor = db["next_cursor"]
 
-    # Return data for use in future steps
     return db
 
 
-def getDatabaseSection(url, token, next_cursor=None):
-    if (next_cursor):
-        payload = {
-            "page_size": 100,
-            "start_cursor": next_cursor
-        }
-    else:
-        payload = {
-            "page_size": 100,
-        }
+def get_database_section(url, token, next_cursor=None):
+    payload = {"page_size": 100}
+    if next_cursor:
+        payload["start_cursor"] = next_cursor
+
     headers = {
         "Authorization": f"Bearer {token}",
         "accept": "application/json",
-        "Notion-Version": "2022-06-28",
+        "Notion-Version": "2023-04-27",
         "content-type": "application/json"
     }
 
@@ -66,27 +50,24 @@ def getDatabaseSection(url, token, next_cursor=None):
     return response.json()
 
 
-def updatePages(token, db, num_random_pages):
-    # Uncheck old pages.
+def update_pages(token, db, num_random_pages):
     for page in db["results"]:
-        if (page["properties"][prop_name]["checkbox"] == True):
-            updatePage(token, page["id"], False)
+        if page["properties"][PROP_NAME]["checkbox"]:
+            update_page(token, page["id"], False)
 
-    # Check new pages
     n = len(db["results"])
-    randIndices = random.sample(range(n), num_random_pages)
+    rand_indices = random.sample(range(n), num_random_pages)
 
-    for index in randIndices:
-        id = db["results"][index]["id"]
-        updatePage(token, id, True)
+    for index in rand_indices:
+        page_id = db["results"][index]["id"]
+        update_page(token, page_id, True)
 
 
-def updatePage(token, page_id, value):
+def update_page(token, page_id, value):
     url = f"https://api.notion.com/v1/pages/{page_id}"
     payload = {
         "properties": {
-            prop_name: {
-                "id": "~ePN",
+            PROP_NAME: {
                 "type": "checkbox",
                 "checkbox": value
             }
@@ -96,39 +77,28 @@ def updatePage(token, page_id, value):
     headers = {
         "Authorization": f"Bearer {token}",
         "accept": "application/json",
-        "Notion-Version": "2022-06-28",
+        "Notion-Version": "2023-04-27",
         "content-type": "application/json"
     }
     response = requests.patch(url, json=payload, headers=headers)
 
 
 def validate_token(token):
-    # Return false if that the token is not of the expected format
-    pattern = r"^secret_[a-zA-Z0-9]{43}$"
-    if not re.match(pattern, token):
-        return False
-    return True
+    return bool(re.match(r"^secret_[a-zA-Z0-9]{43}$", token))
 
 
 def validate_database_id(database_id):
-    # Return false if that the database id is not of the expected format
-    pattern = r"^[a-zA-Z0-9]{32}$"
-    if not re.match(pattern, database_id):
-        return False
-    return True
+    return bool(re.match(r"^[a-zA-Z0-9]{32}$", database_id))
 
 
-'''The main program'''
+'''Main program'''
 
-# Validate the config data
 if not validate_token(INTEGRATION_TOKEN):
     raise ValueError("Invalid token")
 if not validate_database_id(NOTION_DATABASE_ID):
     raise ValueError("Invalid database id")
-if not (isinstance(num_random_pages, int) and 0 < num_random_pages <= 100):
-    raise ValueError(
-        "Invalid number of pages to update. Please enter a number between 1 and 100.")
+if not (isinstance(NUM_RANDOM_PAGES, int) and 0 < NUM_RANDOM_PAGES <= 100):
+    raise ValueError("Invalid number of pages to update. Please enter a number between 1 and 100.")
 
-# Get data from the database and update its pages
-database = queryDatabase(NOTION_DATABASE_ID, INTEGRATION_TOKEN)
-updatePages(INTEGRATION_TOKEN, database, num_random_pages)
+database = query_database(NOTION_DATABASE_ID, INTEGRATION_TOKEN)
+update_pages(INTEGRATION_TOKEN, database, NUM_RANDOM_PAGES)
